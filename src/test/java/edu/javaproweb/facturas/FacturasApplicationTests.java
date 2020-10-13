804ba73664eeaea66fff116ed4c316bf2a1a3ff4
Eduardo-Rivas <edumeru46@gmail.com>
2020-10-12 23:03:27 -0300

    Creación del Proyecto

---
+++ src/test/java/edu/javaproweb/facturas/FacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.javaproweb.facturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
