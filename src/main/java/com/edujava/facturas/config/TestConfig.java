0fefa0377a3260f86a23b2bc54f0f551350c646a
Eduardo-Rivas <edumeru46@gmail.com>
2020-12-06 10:29:58 -0300

    Many-To-Many Association With JoinTable

--- src/main/java/com/edujava/facturas/config/TestConfig.java
+++ src/main/java/com/edujava/facturas/config/TestConfig.java
@@ -53,6 +53,17 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
+		//--Asociacion de Claves Extranjeras Productos-Catgorias--//
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		//--Salvamos Nuevamente la Tabla Producto--//
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
 		//--Instanciamos la Clase User--//
 		User u1 = new User(null, "Edu", "edu@gmail.com", "777", "123");
 		User u2 = new User(null, "Nie", "nieves@gmail.com", "888", "456");
--- src/main/java/com/edujava/facturas/entities/Category.java
+++ src/main/java/com/edujava/facturas/entities/Category.java
@@ -8,8 +8,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_category")
@@ -18,11 +21,13 @@ public class Category implements Serializable{
 	 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long id; 
 	private String name;
 	
 	//--Asociación Entidades Categorias-Productos--//
-	@Transient
+	//--JsonIgnor para Romper el Loop Infinito--//
+	@JsonIgnore 
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
